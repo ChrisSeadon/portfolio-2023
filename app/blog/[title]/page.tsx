4568dc837aac8b5caa3cc7b58c05dd519d822c25
Chris <seadonchristopher@gmail.com>
2024-02-19 00:10:03 +0000

    fixed some 4k+ scaling

--- app/blog/[title]/page.tsx
+++ app/blog/[title]/page.tsx
@@ -8,7 +8,7 @@ export default function Page(props : any){
   const Content = dynamic(() => import(`posts/${title}.mdx`))
   return ( 
     <main>
-      <article className='prose lg:prose-lg xl:prose-xl dark:prose-invert p-4 justify-center items-center m-auto'>
+      <article className='prose lg:prose-lg xl:prose-xl dark:prose-invert p-4 justify-center items-center m-auto 3xl:mt-10 3xl:prose-2xl'>
         <Content/>
       </article>
     </main>
